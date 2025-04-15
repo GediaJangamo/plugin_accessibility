2a56c1c7b270f88108a814176346e2f6e9b993a7
GediaJangamo <gedyahgennyfah@gmail.com>
2025-04-15 10:48:36 +0200

    icone de acessibilidade pronto

--- frontend/src/components/AccessibilityButton.vue
+++ frontend/src/components/AccessibilityButton.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-
-</script>
 
   <template>
     <div class="relative">
@@ -10,15 +6,11 @@
         class="fixed bottom-32 left-0 bg-[#246647] hover:bg-[#579676] text-white py-4 px-6 rounded-r-full shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 z-50 group"
         aria-label="Opcoes de acessibilidade"
       >
-        <img 
-          src="../assets/img/accessibility_icon.png"
-          alt="Icone de acessibilidade"
-          class="h-8 w-8 brightness-0 invert"
-        >
+        <img src="/static/core/img/accessibility_icon.png" alt="Icone de acessibilidade" class="h-8 w-8 brightness-0 invert">
         
         <!-- Tooltip usando apenas Tailwind -->
         <div class="absolute left-full top-1/2 -translate-y-1/2 ml-3 px-3 py-2 bg-gray-800 text-white text-sm rounded whitespace-nowrap invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-300 z-50">
-          <!-- Triângulo/seta do tooltip -->
+           <!-- Triângulo/seta do tooltip - -->
           <div class="absolute top-1/2 right-full -mt-1 border-solid border-8 border-transparent border-r-gray-800"></div>
           Clique para opcoes de acessibilidade
         </div>
@@ -36,3 +28,8 @@
     }
   }
   </script>
+
+  
+
+
+  
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,25 @@
+// const { defineConfig } = require('@vue/cli-service')
+// const path = require('path')
+
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   outputDir: path.resolve(__dirname, '../core/static/core/js'),
+//   filenameHashing: false,
+//   css: {
+//     extract: {
+//       filename: '../css/main.css'
+//     }
+//   },
+//   configureWebpack: {
+//     output: {
+//       filename: 'bundle.js'
+//     },
+//     optimization: {
+//       splitChunks: false // Desabilita a divisão em chunks
+//     }
+//   }
+// })
+
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
@@ -17,5 +39,24 @@ module.exports = defineConfig({
     optimization: {
       splitChunks: false // Desabilita a divisão em chunks
     }
+  },
+  chainWebpack: config => {
+    // Configura o carregamento de imagens
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => {
+        options = options || {}
+        options.limit = 4096 // Arquivos menores que 4kb serão convertidos para base64
+        options.fallback = {
+          loader: 'file-loader',
+          options: {
+            name: '../img/[name].[ext]', // Caminho relativo para imagens
+            publicPath: '/' // Caminho público para acesso às imagens
+          }
+        }
+        return options
+      })
   }
 })
